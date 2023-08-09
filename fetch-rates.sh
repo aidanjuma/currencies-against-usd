b86a413fbbef3c337671b0bff900b7d6cca40aaf
Aidan Juma <aidan@aidanjuma.dev>
2023-08-10 00:07:52 +0100

    🔨 added fetch-rates.sh

---
+++ fetch-rates.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# URL pointing to latest currency rates, measured against the USD.
+# $API_KEY is to be obtainted from GitHub Actions' secret repository!
+RATES_API_URL="https://api.currencybeacon.com/v1/latest?api_key=$API_KEY&base=USD"
+
+# .json to which the currency rates should be saved.
+OUTPUT_FILE="currency-rates.json"
+
+# Fetch rates using curl and store.
+data=$(curl -s "$RATES_API_URL")
+
+if [ $? -eq 0 ]; then
+    # Ensure that there is a response code of 200 within the returned JSON data:
+    response_code=$(echo "$data" | jq -r '.meta.code')
+    if [ "$response_code" -eq 200 ]; then
+        # Get the current UNIX timestamp, and then save with rates_data.
+        timestamp=$(date +%s)
+        rates_data=$(echo "$data" | jq --arg timestamp "$timestamp" '.response | {timestamp: $timestamp | tonumber, base: .base, rates: .rates}')
+        echo "$rates_data" >$OUTPUT_FILE
+        echo "Currency data has been successfully written to currency-rates.json"
+    else
+        echo "Failed to save rates' response to JSON; response code is not 200."
+    fi
+else
+    echo "Failed to fetch latest currency rates from the CurrencyBeacon API."
+fi

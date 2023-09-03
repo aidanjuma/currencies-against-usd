1b41063101b3322064a971f6b8aa2a2d684d1c0c
Aidan Juma <aidan@aidanjuma.dev>
2023-09-03 21:39:20 +0100

    🐛 fixed bug from PR #1 - runs correctly again

--- fetch-rates.sh
+++ fetch-rates.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # URL pointing to latest currency rates, measured against the USD.
-# $API_KEY is to be obtainted from GitHub Actions' secret repository!
+# $API_KEY is to be obtained from GitHub Actions' secret repository!
 RATES_API_URL="https://api.currencybeacon.com/v1/latest?api_key=$API_KEY&base=USD"
 
-# .json to which the currency rates should be saved.
-OUTPUT_FILE="currency-rates.json"
-
-# Fetch rates using curl and store.
-data=$(curl -s "$RATES_API_URL" || echo "Failed to fetch latest currency rates from the CurrencyBeacon API." && exit 1)
+# Fetch rates using curl, or exit gracefully.
+data=$(curl -s "$RATES_API_URL" || (echo "Failed to fetch latest currency rates from the CurrencyBeacon API." && exit 1))
 
 # Ensure that there is a response code of 200 within the returned JSON data:
 response_code=$(echo "$data" | jq -r '.meta.code')
-[ "$response_code" -eq 200 ] || echo "Failed to save rates' response to JSON; response code is not 200." && exit 1
+if [ "$response_code" -ne 200 ]; then
+    echo "Failed to save rates' response to JSON; response code is not 200."
+    exit 1
+fi
 
 # Get the current UNIX timestamp, and then save with rates_data.
 timestamp=$(date +%s)
 rates_data=$(echo "$data" | jq --arg timestamp "$timestamp" '.response | {timestamp: $timestamp | tonumber, base: .base, rates: .rates}')
-echo "$rates_data" >$OUTPUT_FILE
+echo "$rates_data" >"currency-rates.json"
 echo "Currency data has been successfully written to currency-rates.json"
